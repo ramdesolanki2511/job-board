887d64e9dbe959820e3fc340207238e97a764463
ramde solanki <ramdesolanki2511@gmail.com>
2026-07-03 19:20:54 +0530

    feat(auth): implement user registration module

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -2,37 +2,32 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import compression from "compression";
+
+import v1Routes from "./routes/v1";
+
 import { notFound } from "./middlewares/not-found.middleware";
 import { errorHandler } from "./middlewares/error.middleware";
-import v1Routes from "./routes/v1";
 import { v4 as uuid } from "uuid";
 
 const app = express();
 
 app.use(cors());
-
 app.use(helmet());
-
 app.use(compression());
-
 app.use(express.json());
-
-app.use(notFound);
-
-app.use(errorHandler);
-
-app.use("/api/v1", v1Routes);
-
-app.use((req, res, next) => {
-  req.headers["x-request-id"] = uuid();
-  next();
-});
 
 app.get("/", (_, res) => {
   res.json({
     success: true,
     message: "RemoteLaunch API Running 🚀"
   });
+});
+
+app.use("/api/v1", v1Routes);
+
+app.use((req, res, next) => {
+  req.headers["x-request-id"] = uuid();
+  next();
 });
 
 app.get("/health", (_, res) => {
@@ -42,5 +37,8 @@ app.get("/health", (_, res) => {
     timestamp: new Date().toISOString()
   });
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
